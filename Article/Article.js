4a954a749824d5f03921e76ca1e5b8ecedb4a224
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-07-10 15:52:17 -0500

    Article JavaScript working.

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,13 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+
+const articles = document.querySelector('.articles')
+
+// accordion.appendChild(createPanel('hi rosie', 'best cat award'))
+
+Data.forEach(data => {
+  console.log('creating panel:', data.title)
+  accordion.appendChild(createPanel(data.title, data.content))
+})
